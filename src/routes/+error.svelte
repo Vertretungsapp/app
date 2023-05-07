2ae1371996d41186706b85eb50d66050bb6e594a
Sachsenspielt <sachsenspielt3@gmail.com>
2023-05-07 13:51:55 +0200

    feat: error page and more branding

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+import {page} from "$app/stores";
+</script>
+
+<div class="w-full h-screen flex flex-col gap-2 items-center justify-center">
+    <h1 class="text-center">Vertretungsplan<span class="text-accent">42</span></h1>
+    <h1 class=""><span class="text-error">{$page.status}</span> - {$page.error.message}</h1>
+    <a href="/">zurück zur Startseite</a>
+</div>
