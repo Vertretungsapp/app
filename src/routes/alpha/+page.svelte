16b410d840e94f3d166483c4047961cbbc370ea1
Sachsenspielt <sachsenspielt3@gmail.com>
2023-05-07 14:00:28 +0200

    fix: changed unlock mechanism

--- src/routes/alpha/+page.svelte
+++ src/routes/alpha/+page.svelte
@@ -1,6 +1,20 @@
+<script>
+	let count = 0;
+
+	function click() {
+		count++;
+		if (count >= 20) {
+			localStorage.setItem('alphaEnabled', 'true');
+			window.location.href = '/';
+		}
+	}
+</script>
+
 <div class="w-[80%] m-auto mt-20">
 	<h1 class="text-center">Vertretungsplan<span class="text-accent">42</span></h1>
 	<p class="text-center">
 		Diese App steht derzeit nur für eine bestimmte Personengruppe zur Verfügung.
 	</p>
 </div>
+
+<div class="fixed bottom-0 right-0 w-1/3 h-48" on:keypress={() => {}} on:click={click} />
--- src/routes/unlock/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		localStorage.setItem('alphaEnabled', 'true');
-		window.location.href = '/';
-	});
-</script>
