626a13ffa7882292c36e1655151ef395ce109d4f
Sachsenspielt <sachsenspielt3@gmail.com>
2023-05-08 19:20:43 +0200

    feat: added empty settings dialog

--- src/components/LoginAndSettings/LoginAndSettings.svelte
+++ src/components/LoginAndSettings/LoginAndSettings.svelte
@@ -3,14 +3,16 @@
 	import { faKey } from '@fortawesome/free-solid-svg-icons/faKey';
 	import { faGear } from '@fortawesome/free-solid-svg-icons/faGear';
 	import LoginDialog from '../Login/LoginDialog.svelte';
+	import SettingsDialog from "../Settings/SettingsDialog.svelte";
 </script>
 
 <LoginDialog />
+<SettingsDialog />
 
 <div class="flex items-center justify-end gap-2">
 	<LoginAndSettingsIcon
 		icon={faKey}
 		onClick={() => document.querySelector('#loginDialog').showModal()}
 	/>
-	<LoginAndSettingsIcon icon={faGear} />
+	<LoginAndSettingsIcon icon={faGear} onClick={() => document.querySelector('#settingsDialog').showModal()} />
 </div>
---
+++ src/components/Settings/SettingsDialog.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    function closeModal() {
+        document.querySelector('#settingsDialog').close();
+    }
+</script>
+
+<dialog id="settingsDialog" class="bg-background text-text border-accent border-2 rounded-[7px] p-8">
+    <h1 class="my-4 text-center">Einstellungen</h1>
+    <div class="flex flex-col gap-2 items-center my-8">
+        <input type="button" value="Ich bin ein Button" />
+    </div>
+    <input type="button" value="Schließen" on:click={closeModal} />
+</dialog>
+
+<style>
+    dialog::backdrop {
+        background-color: rgba(0, 0, 0, 0.4);
+        backdrop-filter: blur(5px);
+    }
+
+    input {
+        width: 100%;
+    }
+</style>
