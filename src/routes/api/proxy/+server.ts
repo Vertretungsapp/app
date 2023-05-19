77568ab08516028a76371feb03bfc44428aface6
Sachsenspielt <sachsenspielt3@gmail.com>
2023-05-19 17:36:49 +0200

    fix: replaced axios

--- src/routes/api/proxy/+server.ts
+++ src/routes/api/proxy/+server.ts
@@ -1,19 +1,16 @@
 import type { RequestEvent, RequestHandler } from '@sveltejs/kit';
 import { json, text } from '@sveltejs/kit';
-import axios from 'axios';
 
 export const POST: RequestHandler = async ({ request }: RequestEvent) => {
 	const { url, headers } = await request.json();
 
-	const req = await axios
-		.get(url, {
-			headers
-		})
-		.catch((err) => err.response);
+	const req = await fetch(url, {
+		headers
+	})
 
 	if (req.status === 404) return json({}, { status: 404 });
 	if (req.status === 401) return json({}, { status: 401 });
 	if (req.status !== 200) return json({}, { status: 500 });
 
-	return text(req.data, { status: 200 });
+	return text(await req.text(), { status: 200 });
 };
