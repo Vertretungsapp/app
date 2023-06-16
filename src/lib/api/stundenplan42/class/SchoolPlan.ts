56f6bdc1701d7fe4d83d4d2228b805a35e00aa01
Sachsenspielt <sachsenspielt3@gmail.com>
2023-06-16 17:05:49 +0200

    hotfix: one-liner-planinfo was ignored by parser

--- src/lib/api/stundenplan42/class/SchoolPlan.ts
+++ src/lib/api/stundenplan42/class/SchoolPlan.ts
@@ -71,8 +71,9 @@ export function fromJson(schoolnumber: string, json: any): SchoolPlan {
 	if (classes) plan.classes = parseArrayOrObjectFromJson<Class>(classes.Kl, classParser);
 
 	const info = json.VpMobil.ZusatzInfo;
+	if(typeof info.ZiZeile == "string") plan.info = [info.ZiZeile];
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	if (info) plan.info = parseArrayOrObjectFromJson<string>(info.ZiZeile, (s: any) => s);
+	else if (info) plan.info = parseArrayOrObjectFromJson<string>(info.ZiZeile, (s: any) => s);
 
 	plan.generateRooms();
 
