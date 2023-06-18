1ec5e28319b022146cd2a604d7041f5ccda319c0
Sachsenspielt <sachsenspielt3@gmail.com>
2023-06-18 17:14:51 +0200

    feat: added gesture control in planview

--- src/components/Plan/PlanSwitchArrow.svelte
+++ src/components/Plan/PlanSwitchArrow.svelte
@@ -2,31 +2,14 @@
 	import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 	import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 	import { date } from './stores';
-	import addBusinessDays from 'date-fns/addBusinessDays';
 	import Icon from '../Icon.svelte';
+	import { addDays } from '$lib/day';
 
 	export let holidays: Date[];
 	export let turned: boolean;
 
-	/**
-	 * Check if a date is included in an array of dates (ignoring time)
-	 * @param d
-	 * @param dArray
-	 */
-	function isDateIncluded(d: Date, dArray: Date[]): boolean {
-		return dArray.some((date) => {
-			return new Date(date).toLocaleDateString() === d.toLocaleDateString();
-		});
-	}
-
 	function handleClick() {
-		date.update((d) => {
-			let newDate = turned ? addBusinessDays(d, 1) : addBusinessDays(d, -1);
-			while (isDateIncluded(newDate, holidays)) {
-				newDate = turned ? addBusinessDays(newDate, 1) : addBusinessDays(newDate, -1);
-			}
-			return newDate;
-		});
+		date.update((d) => addDays(d, turned ? 1 : -1, holidays));
 	}
 </script>
 
