0b7581fdc92fb82d97e6773bc515451e4ac6e976
Sachsenspielt <sachsenspielt3@gmail.com>
2023-06-30 15:38:04 +0200

    fix: no theme load

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,8 @@
+import { browser } from '$app/environment';
+import { loadTheme } from '$lib/themeSwitcher';
+
 export const prerender = true;
+export const load = async () => {
+	if (!browser) return;
+	loadTheme();
+};
