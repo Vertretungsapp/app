ad997d80250f219716f5e922a2d36c42b947cc96
Sachsenspielt <sachsenspielt3@gmail.com>
2023-06-30 15:37:17 +0200

    chore: faster dark theme load

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,1 @@
-import { browser } from '$app/environment';
-import { loadTheme } from '$lib/themeSwitcher';
-
 export const prerender = true;
-export const load = async () => {
-	if (!browser) return;
-	loadTheme();
-};
