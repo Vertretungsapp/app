4bd94729e892ed99c27d9babb6fdb1dd42388021
Sachsenspielt <sachsenspielt3@gmail.com>
2023-10-06 19:36:30 +0200

    removed useless import

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:package_info_plus/package_info_plus.dart';
