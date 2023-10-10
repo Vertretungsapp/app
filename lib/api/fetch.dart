9ca3b8ca1c9d2d4d760a8f41fb515ffcc2f0f88b
Sachsenspielt <sachsenspielt3@gmail.com>
2023-10-10 17:49:00 +0200

    fix: using proxy to prevent cors errors

--- lib/api/fetch.dart
+++ lib/api/fetch.dart
@@ -5,7 +5,13 @@ import 'package:http/http.dart' as http;
 import 'session.dart';
 
 Future<http.Response> fetch(String url, Credentials credentials) {
-  final response = http
-      .get(Uri.parse(url), headers: {'Authorization': 'Basic ${base64Encode(utf8.encode('${credentials.username.name}:${credentials.password}'))}'});
+  final response = http.post(Uri.parse("https://proxy.vertretungsapp.de"),
+      body: jsonEncode({
+        "url": url,
+        "headers": {
+          'Authorization':
+              'Basic ${base64Encode(utf8.encode('${credentials.username.name}:${credentials.password}'))}'
+        }
+      }));
   return response;
 }
