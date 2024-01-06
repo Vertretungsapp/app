80a98e8a6897c9531d415f64941409c95755b20e
Sachsenspielt <sachsenspielt3@gmail.com>
2024-01-06 20:03:09 +0100

    PWA test

---
+++ TODO.md
@@ -0,0 +1,19 @@
+## V1.0.0
+
+- [ ] Implement Cache cleanup
+- [ ] Implement Plan filter
+- [ ] Implement Plan refresh
+- [ ] Implement favorites
+- [ ] Implement settings
+  - [ ] Implement credentials
+  - [ ] Implement theming (?)
+- [ ] Implement general info on home screen
+- [ ] make PWA
+  - [ ] Implement offline mode
+- [ ] Implement search
+- [ ] Make branding
+
+# V1.1.0
+
+- [ ] Implement desktop mode
+- [ ] Implement find free room (currently or specific time)
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,38 @@
-<script>
+<script lang="ts">
 	import MobileNavigator from '$lib/components/navigation/MobileNavigator.svelte';
 	import '../app.css';
 	import '@fontsource/inter/latin.css';
 	import '@fontsource/poppins/latin.css';
+	import { onMount } from 'svelte';
+
+	import { pwaInfo } from 'virtual:pwa-info';
+
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					// uncomment following code if you want check for updates
+					// r && setInterval(() => {
+					//    console.log('Checking for sw update')
+					//    r.update()
+					// }, 20000 /* 20s for testing purposes */)
+					console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
+
+<svelte:head>
+	{@html webManifestLink}
+</svelte:head>
 
 <div class="flex h-screen max-h-screen w-full flex-col justify-between">
 	<main class="grow overflow-auto px-4 pt-8">
