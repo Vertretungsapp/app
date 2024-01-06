031d346f1c9bb8c69f3e5e1cba37d35efa4fca4d
Sachsenspielt <sachsenspielt3@gmail.com>
2024-01-06 20:15:10 +0100

    Revert "PWA test"
    This reverts commit 80a98e8a6897c9531d415f64941409c95755b20e.

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		SvelteKitPWA({
-			registerType: 'autoUpdate',
-			manifest: {
-				name: 'Vertretungsapp.',
-				short_name: 'Vertretungsapp.',
-				theme_color: '#0549d1',
-				background_color: '#080709',
-				icons: [
-					{
-						src: '/android-chrome-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: '/android-chrome-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				]
-			},
-			devOptions: {
-				enabled: true
-			}
-		})
-	]
+	plugins: [sveltekit()]
 });
