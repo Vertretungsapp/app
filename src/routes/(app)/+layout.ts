ae7be28032801c6f7a907f885f30102391eacb29
Sachsenspielt <sachsenspielt3@gmail.com>
2024-01-13 19:51:55 +0100

    automatic redirect to login

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -1,2 +1,11 @@
+import { goto } from '$app/navigation';
+import { getCredentials } from '$lib/api/session';
+import type { LayoutLoad } from './$types';
+
 export const ssr = false;
 export const prerender = 'auto';
+
+export const load: LayoutLoad = async ({ url }) => {
+	const credentials = getCredentials();
+	if (!credentials && url.pathname != '/settings/credentials') goto('/settings/credentials');
+};
