38e74b74013ace163c5b86e32c5ce183af98a319
Sachsenspielt <sachsenspielt3@gmail.com>
2024-01-24 17:18:18 +0100

    cache clear button (better styling needed)

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
+	import { clearCache } from '$lib/cache/cache';
 	import CredentialsLink from '$lib/components/settings/CredentialsLink.svelte';
+	import toast from 'svelte-french-toast';
+
+	function clrCache() {
+		clearCache();
+		toast.success("Cache geleert", {
+			duration: 2000,
+		});
+	}
 </script>
 
 <div class="flex w-full justify-end"></div>
 
-<div class="flex justify-between">
+<div class="flex justify-between mb-4">
 	<h1>Einstellungen</h1>
 	<CredentialsLink />
 </div>
+
+<button class="bg-secondary-900 p-2 rounded-lg" on:click={clrCache}>Clear Cache</button>
