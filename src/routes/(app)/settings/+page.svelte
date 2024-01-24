dd51d0bb6cd693ed893affc7d93e6fe883a5350e
Sachsenspielt <sachsenspielt3@gmail.com>
2024-01-24 17:24:09 +0100

    Revert "TEMP display cache"
    This reverts commit 401c52d460bc181434ce09f89bd9278a13a254e6.

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { getCredentials } from '$lib/api/session';
-	import { clearCache, getCache } from '$lib/cache/cache';
+	import { clearCache } from '$lib/cache/cache';
 	import CredentialsLink from '$lib/components/settings/CredentialsLink.svelte';
 	import toast from 'svelte-french-toast';
 
@@ -18,11 +17,5 @@
 	<h1>Einstellungen</h1>
 	<CredentialsLink />
 </div>
-
-<pre>
-	<code>
-		{JSON.stringify(getCache(), null, 2)}
-	</code>
-</pre>
 
 <button class="bg-secondary-900 p-2 rounded-lg" on:click={clrCache}>Clear Cache</button>
