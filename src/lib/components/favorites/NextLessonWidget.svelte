74fbe71623ba940d40eeafb72043084570c3d76c
Sachsenspielt <sachsenspielt3@gmail.com>
2024-01-31 17:41:03 +0100

    again additions

--- src/lib/components/favorites/NextLessonWidget.svelte
+++ src/lib/components/favorites/NextLessonWidget.svelte
@@ -25,7 +25,7 @@
     } 
 </script>
 
-<h3 class="flex justify-between items-end">
+<h3 class="flex justify-between items-center">
 	Deine nächsten Stunden
 
 	<select class="bg-background text-primary p-0" on:change={selectEventHandler}>
