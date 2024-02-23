bead4c9b5f0730dd75e5085cafdfe6f10848ba6e
Sachsenspielt <sachsenspielt3@gmail.com>
2024-02-23 15:45:14 +0100

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'de.vertretungsapp.app',
+	appName: 'Vertretungsapp.',
+	webDir: 'build',
+	server: {
+		androidScheme: 'https'
+	}
+};
+
+export default config;
