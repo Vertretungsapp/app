07aaca01e274c7e097b53f9f6b9b964f87a53da9
Sachsenspielt <sachsenspielt3@gmail.com>
2024-04-21 21:16:30 +0200

    fix(migration): migration was started falsely on inital startup
    fix(migration): name now gets displayed right

--- src/lib/migration/migrator.ts
+++ src/lib/migration/migrator.ts
@@ -2,6 +2,7 @@ import { migrators } from '$lib/migration/migratorIndex';
 import toast from 'svelte-french-toast';
 
 export interface Migrator {
+	name: string;
 	version: number;
 	runMigration(): Promise<boolean>;
 }
@@ -20,25 +21,37 @@ export function updateVersion(migrator: Migrator) {
  * @returns void
  */
 export async function runMigrations() {
+	if(Object.keys({...localStorage}).length === 0) {
+		localStorage.setItem('version', getLatestDataVersion().toString());
+	}
+
 	const version = +(localStorage.getItem('version') || 0);
 	for (const migrator of migrators) {
 		if (migrator.version <= version) continue;
 		try {
-			console.debug(`Running migration ${migrator.constructor.name} (v${migrator.version})`);
+			console.debug(`Running migration "${migrator.name}" (v${migrator.version})`);
 			await migrator.runMigration().then((success) => {
 				if (success) {
-					toast.success(`Migration ${migrator.constructor.name} erfolgreich abgeschlossen.`);
+					toast.success(`Migration "${migrator.name}" erfolgreich abgeschlossen.`);
 					console.log(
-						`Migration ${migrator.constructor.name} (v${migrator.version}) completed successfully.`
+						`Migration "${migrator.name}" (v${migrator.version}) completed successfully.`
 					);
 				} else {
-					toast.error(`Migration ${migrator.constructor.name} ist fehlgeschlagen.`);
-					console.error(`Migration ${migrator.constructor.name} (v${migrator.version}) failed.`);
+					toast.error(`Migration "${migrator.name}" ist fehlgeschlagen.`);
+					console.error(`Migration "${migrator.name}" (v${migrator.version}) failed.`);
 				}
 			});
 		} catch (error) {
-			toast.error(`Migration ${migrator.constructor.name} ist fehlgeschlagen.`);
-			console.error(`Migration ${migrator.constructor.name} (v${migrator.version}) failed.`, error);
+			toast.error(`Migration "${migrator.name}" ist fehlgeschlagen.`);
+			console.error(`Migration "${migrator.name}" (v${migrator.version}) failed.`, error);
 		}
 	}
+}
+
+/**
+ * Returns the latest data version
+ * @returns number
+ */
+export function getLatestDataVersion() {
+	return migrators.reduce((max, migrator) => Math.max(max, migrator.version), 0);
 }
