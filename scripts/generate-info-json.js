4c1a24cba82d356e923388d95cea005311ef19e9
Sachsenspielt <sachsenspielt3@gmail.com>
2024-04-27 10:17:01 +0200

    ci: switched to vercel's CI

---
+++ scripts/generate-info-json.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import { execSync } from 'child_process';
+
+// Get version from package.json
+const packageJson = JSON.parse(fs.readFileSync('package.json'));
+const version = packageJson.version;
+
+// Get git SHA
+const gitSha = execSync('git rev-parse --short HEAD').toString().trim();
+
+// Get current date
+const currentDate = new Date();
+
+// Construct JSON object
+const buildInfo = {
+	version: version,
+	sha: gitSha + currentDate.getTime().toString().slice(-3),
+	date: currentDate.toISOString()
+};
+
+// Write JSON object to file
+fs.writeFileSync('static/info.json', JSON.stringify(buildInfo, null, 2), { encoding: 'utf-8' });
+
+console.log('[BUILD] >> Build info JSON file generated successfully.');
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import infoJson from './static/info.json';
+
+const preview = process.env.PREVIEW === "true";
 
 export default defineConfig({
 	plugins: [
@@ -9,9 +12,10 @@ export default defineConfig({
 				org: 'vertretungsapp',
 				project: 'app',
 				authToken: process.env.SENTRY_AUTH_TOKEN,
+				release: preview ? "preview-" + infoJson.sha : infoJson.version + "+" + infoJson.sha,
 				setCommits: {
 					auto: true
-				}
+				},
 			}
 		}),
 		sveltekit()
