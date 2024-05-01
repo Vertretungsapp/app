6dbe5b67e2c91a0714279a384d30f826d179bc41
Finn | Sachsenspielt <59836848+SachsenspieltCoding@users.noreply.github.com>
2024-05-01 14:10:15 +0200

    feat: added buttons with predefined colours in the theming settings (#218)
    * added buttons with predefined colors
    
    * fix eslint

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -2,13 +2,14 @@
 	import { clearCache } from '$lib/cache/cache';
 	import CredentialsLink from '$lib/components/settings/CredentialsLink.svelte';
 	import toast from 'svelte-french-toast';
-	import { PRIMARY_COLOR, settingsStore } from '$lib/stores/settingsStore';
+	import { settingsStore } from '$lib/stores/settingsStore';
 	import SettingsCheckbox from '$lib/components/settings/SettingsCheckbox.svelte';
 	import SettingsSection from '$lib/components/settings/SettingsSection.svelte';
 	import ColorPicker from 'svelte-awesome-color-picker';
 	import SettingsButton from '$lib/components/settings/SettingsButton.svelte';
 	import Icon from '$lib/components/common/Icon.svelte';
 	import { faInfoCircle } from '@fortawesome/free-solid-svg-icons/faInfoCircle';
+	import SettingsThemingDefaultColorSelector from '$lib/components/settings/SettingsThemingDefaultColorSelector.svelte';
 
 	function clrCache() {
 		clearCache();
@@ -37,14 +38,19 @@
 	<SettingsSection>
 		<svelte:fragment slot="title">Darstellung</svelte:fragment>
 
-		<ColorPicker bind:hex={$settingsStore.primaryColor} label="" />
-		<span class="col-span-4">Primärfarbe</span>
+		<div class="bg-display col-span-5 flex items-center gap-2 rounded-lg dark:bg-transparent">
+			<ColorPicker bind:hex={$settingsStore.primaryColor} label="" />
+			<span class="col-span-4">Farbe</span>
+		</div>
 
 		<SettingsCheckbox bind:checked={$settingsStore.darkMode}>Dark Mode</SettingsCheckbox>
 
-		<SettingsButton on:click={() => ($settingsStore.primaryColor = PRIMARY_COLOR)}>
-			Standardfarbe
-		</SettingsButton>
+		<div
+			class="bg-display col-span-full flex flex-col justify-between gap-1 rounded-lg p-2 dark:bg-transparent"
+		>
+			<span>Standardfarben</span>
+			<SettingsThemingDefaultColorSelector />
+		</div>
 	</SettingsSection>
 
 	<SettingsSection>
