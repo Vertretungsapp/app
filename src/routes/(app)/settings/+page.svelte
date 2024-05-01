d4f5255d204368d7a5a45e40334956ab0ad49b69
Sachsenspielt <sachsenspielt3@gmail.com>
2024-05-01 10:16:38 +0200

    only accept json

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -115,7 +115,7 @@
 			<Icon icon={faFileImport} scale={1.1} />
 			Importieren
 		</label>
-		<input id="import_settings_json" type="file" class="hidden" on:input={importSettings} />
+		<input id="import_settings_json" type="file" class="hidden" accept="application/json" on:input={importSettings} />
 	</SettingsSection>
 
 	<SettingsSection>
