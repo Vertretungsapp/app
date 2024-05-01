9affed31a25a5a37ce18ce029c422a41de5d877a
Sachsenspielt <sachsenspielt3@gmail.com>
2024-05-01 10:30:41 +0200

    added splash screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,12 @@
 	import '../app.css';
 	import '@fontsource/inter/latin.css';
 	import '@fontsource/poppins/latin.css';
+	import SplashScreenLinks from '$lib/components/common/SplashScreenLinks.svelte';
 </script>
+
+<svelte:head>
+	<SplashScreenLinks />
+</svelte:head>
 
 <div class="flex h-screen max-h-screen w-full flex-col justify-between">
 	<main class="relative grow overflow-auto px-4 pt-8">
