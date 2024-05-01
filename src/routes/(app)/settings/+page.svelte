2e9ed3d7f8a172e2694b00d170dc687828be01e3
Sachsenspielt <sachsenspielt3@gmail.com>
2024-05-01 10:09:34 +0200

    added functionality

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -16,6 +16,43 @@
 			duration: 2000
 		});
 	}
+
+	function importSettings(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
+		const file = event.currentTarget.files?.[0];
+		if (!file) return;
+
+		const reader = new FileReader();
+		reader.onload = () => {
+			try {
+				const settings = JSON.parse(reader.result as string);
+				settingsStore.set(settings);
+				toast.success('Einstellungen importiert', {
+					duration: 2000
+				});
+			} catch (e) {
+				toast.error('Fehler beim Importieren der Einstellungen', {
+					duration: 2000
+				});
+			}
+		};
+		reader.readAsText(file);
+	}
+
+	onMount(() => {
+		// Get the file contents
+		// let txtFile = `vertretungsapp-settings_${new Date().toISOString().replace(/:/g, '-')}.json`;
+		let str = JSON.stringify($settingsStore);
+
+		// Save the file contents as a DataURI
+		let dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(str);
+
+		// Write it as the href for the link
+		const link = document.getElementById('export_settings_json') as HTMLAnchorElement;
+		if (link) {
+			link.href = dataUri;
+			link.download = `vertretungsapp-settings_${new Date().toISOString().replace(/:/g, '-')}.json`;
+		}
+	});
 </script>
 
 <div class="flex w-full justify-end"></div>
@@ -54,6 +91,28 @@
 			Anonyme Fehlerberichte zur Analyse und Verbesserung der App an Sentry senden (Neustart
 			erforderlich)
 		</SettingsCheckbox>
+	</SettingsSection>
+
+	<SettingsSection>
+		<svelte:fragment slot="title">Im-/Export der Einstellungen</svelte:fragment>
+
+		<a
+			class="bg-clickable col-span-5 flex items-center justify-center gap-2 rounded-lg p-2"
+			href="/settings"
+			id="export_settings_json"
+		>
+			<Icon icon={faFileExport} scale={1.1} />
+			Exportieren
+		</a>
+
+		<label
+			for="import_settings_json"
+			class="bg-clickable col-span-5 flex items-center justify-center gap-2 rounded-lg p-2"
+		>
+			<Icon icon={faFileImport} scale={1.1} />
+			Importieren
+		</label>
+		<input id="import_settings_json" type="file" class="hidden" on:input={importSettings} />
 	</SettingsSection>
 
 	<SettingsSection>
