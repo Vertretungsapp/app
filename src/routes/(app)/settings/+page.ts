67c07dea8af3e0b55b78e6f32e6f47a085f1202f
Sachsenspielt <sachsenspielt3@gmail.com>
2024-05-13 18:17:48 +0200

    format

--- src/routes/(app)/settings/+page.ts
+++ src/routes/(app)/settings/+page.ts
@@ -1,18 +1,18 @@
-import type { PageLoad } from './$types';
 import { browser } from '$app/environment';
+import type { PageLoad } from './$types';
 
 type ServiceWorker = {
 	scope: string;
 	active: string | null;
-}
+};
 
 export const load: PageLoad = async ({ fetch }) => {
 	const response = await fetch('/info.json');
 	const serviceWorkers: ServiceWorker[] = [];
 
-	if(browser && 'serviceWorker' in navigator) {
-		await navigator.serviceWorker.getRegistrations().then(registrations => {
-			for(const registration of registrations) {
+	if (browser && 'serviceWorker' in navigator) {
+		await navigator.serviceWorker.getRegistrations().then((registrations) => {
+			for (const registration of registrations) {
 				serviceWorkers.push({
 					scope: registration.scope,
 					active: registration.active?.state || null
