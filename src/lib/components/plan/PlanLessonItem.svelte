7e994b351237ea2a12790e0c99116e8bebbd0d54
SachsenspieltCoding <coding@sachsenspielt.de>
2024-08-02 20:52:23 +0200

    format code

--- src/lib/components/plan/PlanLessonItem.svelte
+++ src/lib/components/plan/PlanLessonItem.svelte
@@ -50,7 +50,7 @@
 						{slot1.value || ''}
 					</p>
 
-					<p class={twMerge('leading-tight font-medium', slot2.changed && 'text-red-600')}>
+					<p class={twMerge('font-medium leading-tight', slot2.changed && 'text-red-600')}>
 						{slot2.value || ''}
 					</p>
 				</div>
