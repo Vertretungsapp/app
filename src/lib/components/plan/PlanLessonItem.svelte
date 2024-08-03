d9e7b25daa988ebb72caa6141e5b73467292d613
Finn | Sachsenspielt <coding@sachsenspielt.de>
2024-08-03 13:33:43 +0200

    style: Bessere Lesbarkeit des Lehrerkürzels innerhalb einer Stunde (#269)
    * made text in slot 2 lighter of PlanLessonItem
    
    * format code
    
    * grayed out slot 2, revert font-medium
    
    * format code

--- src/lib/components/plan/PlanLessonItem.svelte
+++ src/lib/components/plan/PlanLessonItem.svelte
@@ -50,7 +50,7 @@
 						{slot1.value || ''}
 					</p>
 
-					<p class={twMerge('leading-tight', slot2.changed && 'text-red-600')}>
+					<p class={twMerge('leading-tight text-gray-500', slot2.changed && 'text-red-600')}>
 						{slot2.value || ''}
 					</p>
 				</div>
